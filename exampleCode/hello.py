bb49af1c2891ddac98b50e82b54efcddfa62fbd9
Keith Schneider <keith.schneider811@gmail.com>
2022-11-28 23:33:26 -0500

    adding hello world example

--- exampleCode/hello.py
+++ exampleCode/hello.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ed0cabbfc1cbcae223b4d409859a78921536f115e91040e7817b73281615e98
-size 39
+oid sha256:eed9979879be4d18c16286d9439a2fc7d8744bb887d78bd2987e7fbadeb84a57
+size 20
